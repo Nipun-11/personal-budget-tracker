f9a167855c7eecda00d4add56582e10021590688
Nipun Bansal <savagenipun1@gmail.com>
2025-09-15 02:05:26 +0530

    Fix Git merge conflicts in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-// client/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -18,12 +16,4 @@ export default defineConfig({
       }
     }
   }
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 3c830be (Fixed transactions fetch bug and added budget logic)
 })
